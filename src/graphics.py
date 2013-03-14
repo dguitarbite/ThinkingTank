1a288631bd9b913c6a396d56d3528a0a2c340f14
Pranav Salunke <pps.pranav@gmail.com>
2013-03-14 15:14:49 +0530

    Added a few programs and folders!!! -------------------------------------------------------------------------------- --------------------------------------------------------------------------------
    Details : Finally Decided to use Eclipse to automate unit tests and also
    for the good q and reliable auto complete + parsing. Lets hope that I dont
    end up regretting this.
    
    This project should be completed by Monday Evening 11:59 PM IST - March 18 2013.
    
    --------------------------------------------------------------------------------
    --------------------------------------------------------------------------------
    
    Regards,
    Pranav
    
    --------------------------------------------------------------------------------
    Git Commit From - iamrock - Ubuntu 12.10 amd64 - XPS
    --------------------------------------------------------------------------------

---
+++ src/graphics.py
@@ -0,0 +1,33 @@
+'''
+Created on 14-Mar-2013
+
+@author: iamrock/dguitarbite - dguitarbite@gmail.com
+@license: GPLv3
+@repository: http://www.github.com/ThinkingTank
+'''
+
+
+import pyglet
+from pyglet.window import key
+
+
+class graphics(object):
+    '''
+    
+    This is a heavy Graphics file which contains of the following functionality.
+    
+    1. Call On game startup will lead to starting of the Splash, Logos and then the Main Menu.
+    2. This is kind of like the Graphics Frame work for the game for the sake of handling all the required graphics files.
+     
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    def main_menu(self):
+        '''
+            
+        '''    
---
+++ src/thinkingtank.py
@@ -0,0 +1,157 @@
+#!/usr/bin python2.7 
+
+'''
+Created on 08-Feb-2013
+
+@author: iamrock/dguitarbite - dguitarbite@gmail.com
+@license: GPLv3
+@repository: http://www.github.com/ThinkingTank
+@team : Other team members :
+	Pranav Salunke/dguitarbite - dguitarbite@gmail.com - programming + game design
+	Ritesh Puj - nnsjw702@gmail.com - Documentation + Repository Management
+	Fenil Shah - fenilshah2013@gmail.com - Music + Documentation
+	Ajyanka More - ajinkyamoray@gmail.com - Graphics + Documentation
+	Akshay Ajgaokar - akshayajgaonkar@gmail.com - Graphics + Documentation
+@about: Refer the documentation in the ./docs folder.
+ * ----------------------------------------------------------------------
+ * ---------------------Welcome to Thinking Tank ------------------------
+ * ----------------------------------------------------------------------
+ * 
+
+
+ * This is a simple game which will make your brains work !!!
+ * well I and my team will try our best to match the hype 
+ * Thanks a lot for going through the source code.
+ * 
+ * Just a brief explanation to the Module Structure and break down :
+ *      1) Packages Hierarchy -
+ *              Well I'm at the beginning of Expert Programmer for Python and OOPS
+ *              so I have taken the liberty of skipping Inheritance among the classes.
+ *				that is where I could skip inheritance. There are some method overrides
+ *              but its pyglet not me!!!. I'm just using it.
+ * 
+
+	This game uses Pyglet -- a simpler version of OpenGl for python.
+'''
+
+
+class ThinkingTank :
+	'''
+		This class is responsible for loading various properties, text , images , dependences.
+		This class should also contain the information for levels.
+	'''
+	
+	def __init__ThinkingTank(self):
+		'''
+			/!\	Customized Constructor
+			
+			I'm planning to load a few splash screens, some logos then the graphics.
+			Also this should load the music , then fade in the menu from black.
+			Then its the user who kind of decides what to do with the language.
+		'''
+	
+	def ThinkingTank(self):
+		'''
+			/!\	Customized Constructor
+			
+			I'm planning to load a few splash screens, some logos then the graphics.
+			Also this should load the music , then fade in the menu from black.
+			Then its the user who kind of decides what to do with the language.
+		'''
+		
+
+	def load_sprites(self):
+		'''
+			The basic sprites which will be required for the game:
+			
+				1. Tank
+				2. Rock
+				3. Wall
+				4. Goal Tile
+				6. Normal Tile
+				7. Boundary
+				8. Display Score/Time/Level/Difficulty.
+				9. Background Music
+		'''
+
+		pass
+
+
+	def load_level(self) :
+		'''
+			This game has three levels.
+				This method will load the classes that contain level
+				design, description and other level related code.
+				1. Level 1 - This will be a simple level for introduction to the game. The aim is to warm up your brains.
+				2. Level 2 - This will be a bit tougher, will rack your brains.
+				3. Level 3 - This will be the toughest, grab some coffee!
+		'''
+
+		pass
+
+	
+	def difficulty(self) :
+		'''
+			This game has three difficulty levels.
+			This module will activate or deactivate features as per the difficulty.
+			Difficulty Levels - 
+				1. Noob --> easy.
+					No time and no of moves constraints.
+				2. Geek --> Medium.
+					Time and no of moves will affect the score.
+				3. Einstein --> Difficult.
+					If you don't finish the level in the given
+					time , the player will lose.
+		'''
+
+		pass
+
+
+	def load_music(self) :
+		'''
+			Loads the required music files.
+		'''
+
+		pass 
+
+	def load_graphics(self):
+		'''
+			This method is responsible to load the graphics.
+		'''
+		
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
